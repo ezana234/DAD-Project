6dca6c7ae0081238a97665cd8855ea7b5c733ccc
Ezana <eek9045@rit.edu>
2021-10-24 22:37:05 -0400

    CORS Update 4

--- CFC/backend/main.go
+++ CFC/backend/main.go
@@ -10,10 +10,6 @@ import (
 	"net/http"
 )
 
-func enableCors(w *http.ResponseWriter) {
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-}
-
 type Database struct {
 	database DB.DatabaseConnection
 }
@@ -34,10 +30,10 @@ func main() {
 }
 
 func (db *Database) login(w http.ResponseWriter, r *http.Request) {
-	enableCors(&w)
-	(w).Header().Set("Access-Control-Allow-Origin", "*")
-	(w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 	type Login struct {
 		Email    string
 		Password string
